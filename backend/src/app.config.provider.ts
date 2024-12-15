1629577671e866259f467caabaf5c40b40c2984d
Sergei Gavrilov <iamgavr@yandex.ru>
2024-12-15 22:48:39 +0300

    project sprint-17 completed

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -4,10 +4,9 @@ export const configProvider = {
   imports: [ConfigModule.forRoot()],
   provide: 'CONFIG',
   useValue: <AppConfig>{
-    //TODO прочесть переменнные среды
     database: {
-      driver: process.env.DATABASE_DRIVER || 'mongodb',
-      url: process.env.DATABASE_URL || 'mongodb://localhost:27017/afisha',
+      driver: process.env.DATABASE_DRIVER || 'postgres',
+      url: process.env.DATABASE_URL || 'localhost',
     },
   },
 };
--- backend/src/films/films.service.ts
+++ backend/src/films/films.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { FilmsRepository } from 'src/repository/films.repository';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Film } from './entities/film.entity';
+import { Schedule } from './entities/schedule.entity';
 
 @Injectable()
 export class FilmsService {
-  constructor(private readonly filmsRepository: FilmsRepository) {}
+  constructor(
+    @InjectRepository(Film)
+    private filmsRepository: Repository<Film>,
+  ) {}
 
-  async find() {
-    const films = await this.filmsRepository.find({});
-    const total = films ? films.length : 0;
-    return { total: total, items: films };
+  async find(): Promise<{ total: number; items: Film[] }> {
+    const [total, items] = await Promise.all([
+      this.filmsRepository.count(),
+      this.filmsRepository.find({ relations: { schedule: true } }),
+    ]);
+
+    return { total, items };
   }
 
-  async findOne(id: string) {
-    const film = await this.filmsRepository.findOne({ id });
-    const total = film ? film.schedule.length : 0;
-    return { total: total, items: film.schedule };
+  async findOne(id: string): Promise<{ total: number; items: Schedule[] }> {
+    try {
+      const film = await this.filmsRepository.findOne({
+        where: { id: id },
+        relations: { schedule: true },
+      });
+      return {
+        total: film.schedule.length,
+        items: film.schedule,
+      };
+    } catch (error) {
+      throw new NotFoundException('Фильм не найден');
+    }
   }
 }
